a69a8ffe58a5cad37434f5696c6f0ebf16f8e8d7
JJablonski02 <kuba2805@icloud.com>
2024-01-13 02:15:26 +0100

    main and fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/navigation';
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import fonts from "./src/constants/Font"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+          <Navigation/>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
