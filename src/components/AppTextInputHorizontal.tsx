b181b57cac10d916c268fae42804ceb80edb76ce
JJablonski02 <kuba2805@icloud.com>
2024-01-20 21:38:50 +0100

    components

---
+++ src/components/AppTextInputHorizontal.tsx
@@ -0,0 +1,46 @@
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    TextInputProps,
+    View,
+  } from "react-native";
+  import React, { useState } from "react";
+  import Colors from "../constants/Colors";
+  import Font from "../constants/Font";
+  import FontSize from "../constants/FontSize";
+  import Spacing from "../constants/Spacing";
+  
+  const AppTextInputHorizontal: React.FC<TextInputProps> = ({ ...otherProps }) => {
+    const [focused, setFocused] = useState<boolean>(false);
+    return (
+      <TextInput
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        placeholderTextColor={Colors.darkText}
+        style={[
+          {
+            fontFamily: Font["poppins-regular"],
+            fontSize: FontSize.small,
+            padding: Spacing,
+            backgroundColor: Colors.lightPrimary,
+            borderRadius: Spacing,
+            width: 150
+          },
+          focused && {
+            borderWidth: 3,
+            borderColor: Colors.primary,
+            shadowOffset: { width: 4, height: Spacing },
+            shadowColor: Colors.primary,
+            shadowOpacity: 0.2,
+            shadowRadius: Spacing,
+          },
+        ]}
+        {...otherProps}
+      />
+    );
+  };
+  
+  export default AppTextInputHorizontal;
+  
+  const styles = StyleSheet.create({});
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,19 +1,20 @@
-/**
- * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
-
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import Colors from "../constants/Colors";
-import LoginScreen from "../screens/LoginScreen";
-import Welcome from "../screens/WelcomeScreen";
-import RegisterScreen from "../screens/RegisterScreen";
-import RecoverPasswordScreen from "../screens/RecoverPasswordScreen"
+import LoginScreen from "../screens/auth/LoginScreen";
+import Welcome from "../screens/auth/WelcomeScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
+import RecoverPasswordScreen from "../screens/auth/RecoverPasswordScreen";
+import HomeScreen from "../screens/auth/HomeScreen";
+import PersonalDataScreen from "../screens/auth/PersonalDataScreen";
+import ConfirmAccountScreen from "../screens/auth/ConfirmAccountScreen";
+import MainPage from "../screens/root/MainPage";
 
 import { RootStackParamList } from "../types";
+import { AuthContext, AuthContextProps } from "../context/AuthContext";
+import MainPageScreen from "../screens/root/MainPage";
+import WelcomeScreen from "../screens/auth/WelcomeScreen";
 
 const theme = {
   ...DefaultTheme,
@@ -23,31 +24,93 @@ const theme = {
   },
 };
 
-export default function Navigation() {
-  return (
-    <NavigationContainer theme={theme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
-}
+// export default function Navigation() {
+//   return (
+//     <NavigationContainer theme={theme}>
+//       <RootNavigator />
+//     </NavigationContainer>
+//   );
+// }
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const Navigation = () => {
+  const contextValue: AuthContextProps | undefined = React.useContext(AuthContext)
+  if(contextValue == undefined){
+    console.log("Undefinied context value from index.tsx");
+  }
+return (
+  <NavigationContainer>
+    <Stack.Navigator>
+      {contextValue?.splashLoading ? (
+          <Stack.Screen
+          name='MainPage'
+          component={MainPageScreen}
+          options={{headerShown: false}}
+        />
+      ) : contextValue?.userInfo.access_token ? (
+        <Stack.Screen name="Home" component={HomeScreen} />
+      ) : (
+        <Stack.Group screenOptions={{
+          headerShown: false
+        }}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+          <Stack.Screen name='Login' component={LoginScreen}/>
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+          />
+          <Stack.Screen
+          name="RecoverPassword"
+          component={RecoverPasswordScreen}
+          />
+        </Stack.Group>
+      )}
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+};
 
 /**
  * A root stack navigator is often used for displaying modals on top of all other content.
  * https://reactnavigation.org/docs/modal
  */
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-function RootNavigator() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-        <Stack.Screen name="Welcome" component={Welcome}/>
-        <Stack.Screen name = "Login" component={LoginScreen} />
-        <Stack.Screen name = "Register" component={RegisterScreen} />
-        <Stack.Screen name = "RecoverPassword" component={RecoverPasswordScreen}/>
-    </Stack.Navigator>
-  );
-}
+// const AuthStack = createNativeStackNavigator<RootStackParamList>();
+// function RootNavigator() {
+//   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+//   const handleLoginSuccess = () => {
+//     setIsAuthenticated(true);
+//   }
+//   return (
+//     <NavigationContainer></NavigationContainer>
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         {isAuthenticated ? (
+//           //Logged users
+//           <Stack.Group
+//             screenOptions={{
+//               headerShown: true,
+//             }}
+//           >
+//             <Stack.Screen name="MainPage" component={MainPage} />
+//           </Stack.Group>
+//         ) : (
+//            //Auth screens
+//           <Stack.Group
+//             screenOptions={{
+//               headerShown: false,
+//             }}
+//           >
+//             <Stack.Screen name="Welcome" component={Welcome}/>
+//             <Stack.Screen name="Login" component={LoginScreen}/>
+//             <Stack.Screen name="Register" component={RegisterScreen} />
+//             <Stack.Screen name="RecoverPassword" component={RecoverPasswordScreen} />
+//             <Stack.Screen name="Home" component={HomeScreen} />
+//             <Stack.Screen name="PersonalData" component={PersonalDataScreen} />
+//             <Stack.Screen name="ConfirmAccount" component={ConfirmAccountScreen} />
+//           </Stack.Group>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
