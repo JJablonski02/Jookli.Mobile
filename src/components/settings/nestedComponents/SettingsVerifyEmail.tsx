ffdf1fd0e86e63c76896c8649ed7655f014365d4
JJablonski02 <kuba2805@icloud.com>
2024-02-01 22:39:27 +0100

    settings verify email component

---
+++ src/components/settings/nestedComponents/SettingsVerifyEmail.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Touchable, Text, Switch, StyleSheet} from "react-native";
+
+const SettingsVerifyEmail = () => {
+    return(
+        <View>
+            <Text>
+                Verify your email address
+            </Text>
+        </View>
+    );
+};
+
+export default SettingsVerifyEmail;
+
+const styles = StyleSheet.create({
+    
+});
