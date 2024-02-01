10542a344c8f5ab99a732cde03d09355f54a1acc
JJablonski02 <kuba2805@icloud.com>
2024-02-01 22:39:16 +0100

    settings profile component

---
+++ src/components/settings/SettingsProfileComponent.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Image,
+    TouchableOpacity 
+} from 'react-native';
+import Colors from '../../constants/Colors';
+import Spacing from '../../constants/Spacing';
+
+
+const SettingsProfileComponent : React.FC = () => {
+    return(
+        <View style={styles.profile}>
+          <View style={styles.profileHeader}>
+            <Image
+              alt=""
+              source={require('../../../assets/images/bitmoji.png')}
+              style={styles.profileAvatar}
+              resizeMode="contain" />
+
+            <View>
+              <Text style={styles.profileName}>John Doe</Text>
+
+              <Text style={styles.profileHandle}>@john.doe</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <View style={styles.profileAction}>
+              <Text style={styles.profileActionText}>Edit Profile</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+    );
+};
+
+export default SettingsProfileComponent;
+
+const styles = StyleSheet.create({
+      profile: {
+        paddingTop: 12,
+        paddingHorizontal: 24,
+        paddingBottom: 24,
+        backgroundColor: '#fff',
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#e3e3e3',
+      },
+      profileHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      },
+      profileAvatar: {
+        width: 60,
+        height: 60,
+        borderRadius: 1000,
+        marginRight: 12,
+      },
+      profileName: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: '#3d3d3d',
+      },
+      profileHandle: {
+        marginTop: 4,
+        fontSize: 15,
+        color: '#989898',
+      },
+      profileAction: {
+        marginTop: 16,
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.primary,
+        borderRadius: Spacing * 2,
+      },
+      profileActionText: {
+        marginRight: 8,
+        fontSize: 15,
+        fontWeight: '600',
+        color: '#fff',
+      },
+});
