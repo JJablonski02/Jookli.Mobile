45ab3b7781ec5b285b6a68cbed6d45326ba3c33e
JJablonski02 <kuba2805@icloud.com>
2024-02-02 23:06:28 +0100

    settings preference component

--- src/components/settings/SettingsPreferencesComponent.tsx
+++ src/components/settings/SettingsPreferencesComponent.tsx
@@ -1,13 +1,70 @@
 import React from "react";
-import { View, Touchable, Text, Switch, StyleSheet} from "react-native";
+import {
+  View,
+  Touchable,
+  Text,
+  Switch,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import SettingsVerifyEmail from "./nestedComponents/SettingsVerifyEmail";
+import SettingsTextInput from "./nestedComponents/SettingsTextInput";
+import Spacing from "../../constants/Spacing";
+import Colors from "../../constants/Colors";
+import FontSize from "../../constants/FontSize";
+import { TextInput } from "react-native-paper";
+import SettingsDropdown from "./nestedComponents/SettingsDropdown";
+import SettingsDisplayProfitOne from "./nestedComponents/SettingsDisplayProfitOne";
 
-const SettingsPreferencesComponent = () => {
+const SettingsPreferencesComponent: React.FC = () => {
   return (
-    <View>
-        <SettingsVerifyEmail/>
+    <View style={styles.container}>
+      <SettingsVerifyEmail />
+      <Text style={styles.title}>Change email address</Text>
+      <SettingsTextInput />
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.textContainer}>Save</Text>
+      </TouchableOpacity>
+      <Text style={styles.title}>Language and time zone</Text>
+      <SettingsDropdown/>
+      <SettingsDropdown/>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.textContainer}>Save</Text>
+      </TouchableOpacity>
+      <SettingsDisplayProfitOne/>
     </View>
   );
-}
+};
 
 export default SettingsPreferencesComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Spacing * 2,
+    marginHorizontal: Spacing * 3,
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: "PoppinsBold",
+    marginTop: Spacing * 2,
+  },
+  section: {
+    marginTop: Spacing,
+  },
+  buttonContainer: {
+    elevation: 5,
+    padding: 3,
+    borderRadius: Spacing * 2,
+    backgroundColor: Colors.primary,
+    marginTop: Spacing * 2,
+    width: 200,
+    shadowOpacity: 0.5,
+    alignSelf: "center",
+  },
+  textContainer: {
+    textAlign: "center",
+    color: Colors.background,
+    fontSize: FontSize.medium,
+    fontWeight: "bold",
+  },
+});
