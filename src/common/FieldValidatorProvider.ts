0f2da2ef1a735a34467489e75072e1cc8ffd8402
JJablonski02 <kuba2805@icloud.com>
2024-02-12 22:21:55 +0100

    Field validator provider

---
+++ src/common/FieldValidatorProvider.ts
@@ -0,0 +1,168 @@
+const isFirstNameValid = (text: string) => {
+    const firstName = text;
+    if (!firstName) {
+      return false;
+    }
+    const onlyLetters = /^[A-Za-z]+$/;
+    return onlyLetters.test(firstName);
+  };
+
+  const isLastNameValid = (text: string) => {
+    const lastName = text;
+
+    if (!lastName) {
+      return false;
+    }
+    const onlyLetters = /^[A-Za-z]+$/;
+    return onlyLetters.test(lastName);
+  };
+
+  const isStreetValid = (text: string) => {
+    const street = text;
+
+    if (!street) {
+      return false;
+    }
+    const onlyLetters = /^[A-Za-z]+$/;
+    return onlyLetters.test(street);
+  };
+
+  const isHouseNumberValid = (text: string) => {
+    const houseNumber = text;
+
+    if (!houseNumber) {
+      return false;
+    }
+    const onlyLetters = /^[0-9A-Za-z/]+$/;
+    return onlyLetters.test(houseNumber);
+  };
+
+  const isZipCodeValid = (text: string) => {
+    const zipCode = text;
+
+    if (!zipCode) {
+      return false;
+    }
+    const onlyLetters = /^[0-9-]+$/;
+    return onlyLetters.test(zipCode);
+  };
+
+  const isCityValid = (text: string) => {
+    const city = text;
+
+    if (!city) {
+      return false;
+    }
+    const onlyLetters = /^[A-Za-z]+$/;
+    return onlyLetters.test(city);
+  };
+
+  const isStateRegionValid = (text: string) => {
+    const stateRegion = text;
+
+    if (!stateRegion) {
+      return false;
+    }
+    const onlyLetters = /^[A-Za-z]+$/;
+    return onlyLetters.test(stateRegion);
+  };
+  const isAreaCodeSelected = (text: string) => {
+    const areaCode = text;
+
+    if(areaCode){
+      return true;
+    }
+
+    return false;
+  };
+
+  const isPhoneNumberValid = (text: string) => {
+    const phoneNumber = text;
+
+    if (!phoneNumber) {
+      return false;
+    }
+    const onlyLetters = /^[0-9-]+$/;
+    return onlyLetters.test(phoneNumber);
+  };
+
+  
+interface PaymentDetails {
+    FirstName: string;
+    LastName: string;
+    Street: string;
+    HouseNumber: string;
+    ZipCode: string;
+    City: string;
+    StateRegion: string;
+    Currency: string;
+    Country: string;
+  }
+
+  export const ValidatePaymentsComponent : (paymentDetails : PaymentDetails) => string | null = (paymentDetails) =>{
+    switch (true) {
+      case !isFirstNameValid(paymentDetails.FirstName):
+        return "First name is not valid";
+      case !isLastNameValid(paymentDetails.LastName):
+        return "Last name is not valid";
+      case !isStreetValid(paymentDetails.Street):
+        return "Street is not valid";
+      case !isHouseNumberValid(paymentDetails.HouseNumber):
+        return "House number is not valid";
+      case !isZipCodeValid(paymentDetails.ZipCode):
+        return "Zip code is not valid";
+      case !isCityValid(paymentDetails.City):
+        return "City is not valid";
+      case !isStateRegionValid(paymentDetails.StateRegion):
+        return "State or region is not valid";
+      default:
+        return null;
+    }
+  };
+
+  interface ContactProps {
+    FirstName: string;
+    LastName: string;
+    AreaCode: string;
+    PhoneNumber: string;
+  };
+
+  export const ValidatePrivacyContactComponent : (contactProps : ContactProps) => string | null = (contactProps) =>{
+    switch (true) {
+      case !isFirstNameValid(contactProps.FirstName):
+        return "First name is not valid";
+      case !isLastNameValid(contactProps.LastName):
+        return "Last name is not valid";
+        case !isAreaCodeSelected(contactProps.AreaCode):
+            return "Area code is not selected";
+      case !isPhoneNumberValid(contactProps.PhoneNumber):
+        return "Phone number is not valid";
+      default:
+        return null;
+    }
+  };
+
+  interface NotificationProps{
+    TaskCompleted: boolean;
+    TransferConfirmation: boolean;
+    NewRegistration: boolean;
+    MoneyFromReferral: boolean;
+    OccasionalEvents: boolean;
+  };
+  
+  export const ValidatePrivacyNotificationsComponent : (notificationProps : NotificationProps) => string | null = (notificationProps) =>{
+    console.log(notificationProps);
+    return null;
+  }
+
+  interface ReceivingMessagesProps{
+    PersonalizedHelp: boolean;
+    PeriodicNewsletters: boolean;
+    OccasionalSurveys: boolean;
+    SpecialOffers: boolean;
+  };
+
+  export const ValidatePrivacyMessagesComponent : (receivingMessagesProps : ReceivingMessagesProps) => string | null = (receivingMessagesProps) =>{
+    console.log(receivingMessagesProps);
+    return null;
+  };
