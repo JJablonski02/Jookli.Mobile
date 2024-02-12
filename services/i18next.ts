9b5f0964a3aeeca2f58ffc0426229443f8063422
JJablonski02 <kuba2805@icloud.com>
2024-02-12 22:49:31 +0100

    i18next parameter

--- services/i18next.ts
+++ services/i18next.ts
@@ -2,7 +2,7 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "../locales/en.json";
 import sv from "../locales/sv.json";
-import pl from "../locales/pl.json";
+import pl_PL from "../locales/pl.json";
 import da from "../locales/da.json";
 import de from "../locales/de.json";
 import ar from "../locales/ar.json";
@@ -10,7 +10,7 @@ import ar from "../locales/ar.json";
 
 export const languageResources = {
     en: {translation: en},
-    pl: {translation: pl},
+    pl: {translation: pl_PL},
     sv: {translation: sv},
     da: {translation: da},
     de: {translation: de},
@@ -21,7 +21,8 @@ i18next.use(initReactI18next).init({
     compatibilityJSON: 'v3',
     lng: 'en',
     fallbackLng: 'en',
+    lowerCaseLng: true,
     resources: languageResources,
-})
+});
 
 export default i18next;
