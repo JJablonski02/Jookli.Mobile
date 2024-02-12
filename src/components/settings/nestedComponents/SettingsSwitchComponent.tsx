ba8277945e807247dac3b164d46f24976fce0a31
JJablonski02 <kuba2805@icloud.com>
2024-02-12 22:50:32 +0100

    settings swtich component adjustments

--- src/components/settings/nestedComponents/SettingsSwitchComponent.tsx
+++ src/components/settings/nestedComponents/SettingsSwitchComponent.tsx
@@ -5,11 +5,19 @@ import Colors from "../../../constants/Colors";
 
 interface Props{
     label?: string;
+    isSwitchEnabled?: (isEnabled: boolean) => void;
 }
 
 const SettingsSwitchComponent : React.FC<Props> = (props) => {
     const [isEnabled, setEnabled] = React.useState(false);
-    const toggleSwitch = () => setEnabled(previousState => !previousState);
+    const toggleSwitch = () => {
+        setEnabled(previousState => !previousState);
+
+        if(props.isSwitchEnabled){
+            props.isSwitchEnabled(!isEnabled);
+        }
+    };
+
     return (
     <View style={styles.container}>
         {props.label && <Text style={styles.description}>{props.label}</Text>}
