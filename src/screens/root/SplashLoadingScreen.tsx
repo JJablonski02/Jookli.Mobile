534fa613e89b610e24a1e1f0c23d39e5af4ef8c0
JJablonski02 <kuba2805@icloud.com>
2024-02-17 11:37:32 +0100

    Splash

--- src/screens/root/SplashLoadingScreen.tsx
+++ src/screens/root/SplashLoadingScreen.tsx
@@ -1,10 +1,11 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types";
-import { StyleSheet, Text, Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import AppLoading from "expo-app-loading";
 import CustomSafeAreaView from "../../components/SafeViewAndroid";
 import { ActivityIndicator } from "react-native-paper";
 import Colors from "../../constants/Colors";
+import TextV from "../../components/global/Text";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'SplashLoading'>;
 
@@ -12,7 +13,7 @@ const SplashLoadingScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
     return (
         <CustomSafeAreaView style={styles.container}>
             <Image src="../../assets/images/JoyProfits_Welcome.png" width={60} height={60} />
-            <Text>Loading...</Text>
+            <TextV>Loading...</TextV>
             <ActivityIndicator size={'large'} color={Colors.primary}/>
         </CustomSafeAreaView>
     );
