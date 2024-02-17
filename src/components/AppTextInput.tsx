9bd6917b23e207743699626ea9a892549124f219
JJablonski02 <kuba2805@icloud.com>
2024-02-17 12:48:22 +0100

    app text input

--- src/components/AppTextInput.tsx
+++ src/components/AppTextInput.tsx
@@ -7,18 +7,20 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import Colors from "../constants/Colors";
-import Font from "../constants/Font";
 import FontSize from "../constants/FontSize";
 import Spacing from "../constants/Spacing";
 
 const AppTextInput: React.FC<TextInputProps> = ({ ...otherProps }) => {
   const [focused, setFocused] = useState<boolean>(false);
-  const [email ,setEmail] = useState(null);
+
   return (
     <TextInput
       onFocus={() => setFocused(true)}
       onBlur={() => setFocused(false)}
       placeholderTextColor={Colors.darkText}
+      allowFontScaling={false}
+      autoCorrect={false}
+      autoComplete="off"
       style={[
         {
           fontFamily: "PoppinsRegular",
