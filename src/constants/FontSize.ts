d7a72d9f7bd9d67fddf8c280f7a45b477b9f2363
JJablonski02 <kuba2805@icloud.com>
2024-02-17 11:40:04 +0100

    Created fontsize automat scaler

--- src/constants/FontSize.ts
+++ src/constants/FontSize.ts
@@ -1,3 +1,5 @@
+import { Dimensions } from 'react-native';
+
 const xSmall: number = 10;
 const small: number = 14;
 const medium: number = 16;
@@ -13,3 +15,13 @@ export default{
     xLarge,
     xxLarge,
 }
+const {width, height} = Dimensions.get('window');
+
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scale = (size : number)  => width / guidelineBaseWidth * size;
+const verticalScale = (size : number) => height / guidelineBaseHeight * size;
+const moderateScale = (size : number, factor = 0.5) => size + ( scale(size) - size ) * factor;
+
+export {scale, verticalScale, moderateScale};
