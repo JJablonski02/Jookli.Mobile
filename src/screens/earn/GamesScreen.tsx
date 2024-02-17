8958d5faa22b1f65377795a0421e49c01532f8b3
JJablonski02 <kuba2805@icloud.com>
2024-02-17 11:38:42 +0100

    games screen cleanup

--- src/screens/earn/GamesScreen.tsx
+++ src/screens/earn/GamesScreen.tsx
@@ -1,13 +1,14 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types";
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View } from "react-native";
+import TextV from "../../components/global/Text";
 
 
 const GamesScreen: React.FC = () => {
     return (
         <SafeAreaView>
             <View>
-                <Text>Games Screen</Text>
+                <TextV>Games Screen</TextV>
             </View>
         </SafeAreaView>
     );
