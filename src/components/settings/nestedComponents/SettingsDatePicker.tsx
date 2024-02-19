bd71215509ff4dee6226cf7c564a5cf1bf7aff28
JJablonski02 <kuba2805@icloud.com>
2024-02-19 23:28:22 +0100

    Settings Date picker

--- src/components/settings/nestedComponents/SettingsDatePicker.tsx
+++ src/components/settings/nestedComponents/SettingsDatePicker.tsx
@@ -1,8 +1,124 @@
-import React, { useState } from 'react'
-import DatePicker from 'react-native-date-picker'
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import React, { useState } from "react";
+import { Button, View, StyleSheet, TouchableOpacity, Platform } from "react-native";
+import { moderateScale, scale } from "../../../constants/FontSize";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import TextV from "../../global/Text";
+import Spacing from "../../../constants/Spacing";
 
-export default () => {
-  const [date, setDate] = useState(new Date())
-
-  return <DatePicker date={date} onDateChange={setDate} />
+interface Props {
+    label?: string;
 }
+
+
+const SettingsDatePicker: React.FC<Props> = ({label}) => {
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [day, setDay] = useState<string>("23");
+    const [month, setMonth] = useState<string>("04");
+    const [year, setYear] = useState<string>("2023");
+
+    const showDatePicker = () => {
+      setDatePickerVisibility(true);
+    };
+  
+    const hideDatePicker = () => {
+      setDatePickerVisibility(false);
+    };
+  
+    const handleConfirm = (date : Date) => {
+        const year = date.getFullYear().toString();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+      hideDatePicker();
+    };
+
+    return (
+      <View>
+        <View style={styles.container}>
+            <View>
+            <TextV style={labelStyles.labelStyle}>Day</TextV>
+            <TouchableOpacity style={Platform.OS==='ios'?styles.inputIOS : styles.inputAndroid} activeOpacity={1.0} onPress={showDatePicker}>
+                <TextV>{day}</TextV>
+                <Ionicons name="chevron-down" size={24} color="black" style={Platform.OS === 'ios' ? styles.inputIOS2 : styles.inputAndroid2}/>
+            </TouchableOpacity>
+            </View>
+            <View>
+            <TextV style={labelStyles.labelStyle}>Month</TextV>
+            <TouchableOpacity style={Platform.OS==='ios'?styles.inputIOS : styles.inputAndroid} activeOpacity={1.0} onPress={showDatePicker}>
+                <TextV>{month}</TextV>
+                <Ionicons name="chevron-down" size={24} color="black" style={Platform.OS === 'ios' ? styles.inputIOS2 : styles.inputAndroid2}/>
+            </TouchableOpacity>
+            </View>
+            <View>
+            <TextV style={labelStyles.labelStyle}>Year</TextV>
+            <TouchableOpacity style={Platform.OS==='ios'?styles.inputIOS : styles.inputAndroid} activeOpacity={1.0} onPress={showDatePicker}>
+                <TextV>{year}</TextV>
+                <Ionicons name="chevron-down" size={24} color="black" />
+            </TouchableOpacity>
+            </View>
+        </View>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          display="spinner"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+      </View>
+    );
+};
+
+export default SettingsDatePicker;
+
+const labelStyles = StyleSheet.create({
+    labelStyle:{
+        fontSize: moderateScale(14),
+        fontFamily: "PoppinsRegular",
+        marginBottom: 5,
+        marginTop:20,    
+    },
+});
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        width: "100%",
+    },
+    inputIOS: {
+        fontSize: moderateScale(14),
+        borderWidth: 1,
+        borderColor: '#E0E0E0',
+        borderRadius: 4,
+        color: 'black',
+        fontFamily: "PoppinsRegular",
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: moderateScale(10),    
+        paddingHorizontal: moderateScale(10),  
+    },
+    inputIOS2:{
+        marginLeft: scale(20),
+    },
+    inputAndroid: {
+        fontSize: moderateScale(14),
+        borderWidth: 1,
+        borderColor: '#E0E0E0',
+        borderRadius: 30,
+        color: 'black',
+        fontFamily: "PoppinsRegular",
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingVertical: moderateScale(10),
+        paddingHorizontal: moderateScale(10),
+    },
+    inputAndroid2:{
+        marginLeft: scale(20),
+    }
+});
