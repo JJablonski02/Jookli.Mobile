6f3e6e7a980b6414ef38f851bc71dbf3a33496f8
Jakub Jabłoński <jakub.jablonski@lstsoft.com.pl>
2024-02-23 23:37:45 +0100

    settings dropdown

--- src/components/settings/nestedComponents/SettingsDropdown.tsx
+++ src/components/settings/nestedComponents/SettingsDropdown.tsx
@@ -28,7 +28,7 @@ const SettingsDropdown : React.FC<Props> = ({placeholder, label, dataSource, sel
                 }}
                 useNativeAndroidPickerStyle={false}
                 Icon={() => {
-                    return <Ionicons name="chevron-down" size={24} style={{top: 10, right: 20}}/>;
+                    return <Ionicons name="chevron-down" size={24}/>;
                 }}
                 />
         </View>
@@ -53,17 +53,17 @@ const labelStyles = StyleSheet.create({
 
 const pickerSelectStyles = StyleSheet.create({
     inputIOS: {
-        fontSize: moderateScale(14),
+        fontSize: moderateScale(13),
         borderWidth: 1,
         borderColor: '#E0E0E0',
         borderRadius: 4,
         color: 'black',
         fontFamily: "PoppinsRegular",
-        paddingHorizontal: moderateScale(20),
+        paddingHorizontal: moderateScale(10),
         paddingVertical: moderateScale(6),
     },
     inputAndroid: {
-        fontSize: moderateScale(14),
+        fontSize: moderateScale(13),
         paddingHorizontal: moderateScale(20),
         paddingVertical: moderateScale(6),
         borderWidth: 1,
@@ -73,10 +73,14 @@ const pickerSelectStyles = StyleSheet.create({
         fontFamily: "PoppinsRegular",
     },
     placeholder:{
-        fontSize: moderateScale(14),
+        fontSize: moderateScale(13),
         fontFamily: "PoppinsRegular",
-        paddingLeft: 10,
+        paddingRight: moderateScale(45)
     },
+    iconContainer:{
+        top: moderateScale(7),
+        right: moderateScale(10)
+    }
 });
 
 export default SettingsDropdown;
