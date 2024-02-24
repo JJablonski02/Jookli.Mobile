4d6a79e5f889b50f85ae1d30cf356e6a29269042
Jakub Jabłoński <jakub.jablonski@lstsoft.com.pl>
2024-02-24 22:17:25 +0100

    education json data

--- src/components/settings/SettingsInfoComponent.tsx
+++ src/components/settings/SettingsInfoComponent.tsx
@@ -12,6 +12,7 @@ import CountryPhoneCodes from "../../../locales/options/countryPhoneCodes.json";
 import { moderateScale } from "../../constants/FontSize";
 import { SettingsSafeView } from "./nestedComponents/SettingsSafeView";
 import SettingsModalView from "./nestedComponents/SettingsModalView";
+import Education from "../../../locales/options/education.json"
 
 const DATASOURCE = [
   { label: 'Other', value: '0' },
@@ -34,6 +35,17 @@ const fetchCountryData = () => {
   return data;
 }
 
+const fetchEducationData = () => {
+  const data: Props[] =  Object.keys(Education.FinishedSchools).map((key: string) => {
+    return {
+      label: Education.FinishedSchools[key as keyof typeof Education.FinishedSchools],
+      value: key,
+    };
+  });
+
+  return data;
+};
+
 
 
 const SettingsInfoComponent: React.FC = () => {
@@ -51,14 +63,16 @@ const SettingsInfoComponent: React.FC = () => {
       <SettingsButtonSave/>
       <View style={{marginTop: Spacing * 2}}>
         <TextV style={styles.title}>Experience</TextV>
-        <SettingsModalView label="Education" placeholder="Select degree..."/>
-        <SettingsDropdown/>
+        <TextV style={styles.description}>Add your work experience to get higher paying revenue generating ads</TextV>
+        <SettingsModalView label="Education" placeholder="Select degree..." dataSource = {fetchEducationData()}/>      
+        <SettingsModalView label="Specialisation" placeholder="Select specialisation..." dataSource={fetchCountryData()}/>
         <SettingsButtonSave/>
       </View>
       <View style={{marginVertical: Spacing * 2}}>
         <TextV style={styles.title}>Subcategories</TextV>
-        <SettingsDropdown/>
-        <SettingsDropdown/>
+        <TextV style={styles.description}>Allow yourself to see ads based only on what you like and who you are</TextV>
+        <SettingsModalView label="Additional personal informations" placeholder="Nothing selected" dataSource={fetchEducationData()}/>
+        <SettingsModalView label="Interests" placeholder="Nothing selected" dataSource={fetchEducationData()}/>
         <SettingsButtonSave/>
       </View>
     </View>
