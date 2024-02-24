c74154256422be3c3d2820005f19c3e42504a5ec
Jakub Jabłoński <jakub.jablonski@lstsoft.com.pl>
2024-02-24 22:18:03 +0100

    shopping screen

---
+++ src/screens/earn/ShoppingScreen.tsx
@@ -0,0 +1,30 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { EarnStackParamList, RootStackParamList } from "../../types";
+import { SafeAreaView, View } from "react-native";
+import TextV from "../../components/global/Text";
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+type EarnStackScreenProps = NativeStackScreenProps<EarnStackParamList, 'Shopping'>;
+
+const ShoppingScreen: React.FC<EarnStackScreenProps> = () => {
+    const navigation = useNavigation().getParent();
+
+    const handleOnPress = () => {
+         navigation?.setOptions({
+            headerShown: true
+         })
+    }
+
+    return (
+        <SafeAreaView>
+            <View>
+            <TouchableOpacity onPress={handleOnPress}>
+                <TextV style={{justifyContent: 'center', alignSelf: 'center', }}>ShoppingScreen</TextV>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default ShoppingScreen;
