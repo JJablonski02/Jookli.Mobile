7ed2382b8c99ff1f85a264b94fdd4de6e9215208
JJablonski02 <kuba2805@icloud.com>
2024-02-25 21:19:04 +0100

    fyber offerwall

---
+++ src/screens/earn/offerwalls/FyberOfferwallScreen.tsx
@@ -0,0 +1,30 @@
+import 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+} from 'react-native';
+import {AyetOfferwall} from 'ayetsdk';
+import {NavigationContainer} from '@react-navigation/native';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { EarnStackParamList } from '../../../types';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type EarnStackScreenProps = NativeStackScreenProps<EarnStackParamList, 'Fyber'>;
+
+const FyberOfferwallScreen : React.FC<EarnStackScreenProps> = ({navigation}) => {
+    const onClose = () => {
+        const parentNavigator = navigation.getParent();
+        if(parentNavigator){
+        navigation.navigate('Games');
+        parentNavigator.setOptions({headerShown: true})
+        };
+    };
+    
+    return <AyetOfferwall userId="12345" adslotId="15982" onClose={onClose} />
+};
+
+export default FyberOfferwallScreen;
