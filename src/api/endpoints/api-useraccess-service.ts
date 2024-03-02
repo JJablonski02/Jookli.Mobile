694947f90fbd24f6d13c1ef5fdb87f9edefce9ed
Jakub Jabłoński <jakub.jablonski@lstsoft.com.pl>
2024-03-02 23:11:50 +0100

    useraccess-service endpoints

--- src/api/endpoints/api-useraccess-service.ts
+++ src/api/endpoints/api-useraccess-service.ts
@@ -1,29 +1,36 @@
-import { readonly } from "../../helpers/readonly";
 import { API } from "../core/api-connection";
 import axios from "axios";
 import {RegisterUserDTO} from '@api';
+import { REACT_APP_API_URL } from "@env";
 
-const DOMAIN = readonly({
-    path: '/useraccess'
 
-});
-
+//Sends request on user register
 export const registerUser = async (userModel: RegisterUserDTO) => {
     try{
-        const response = await API.post(`${DOMAIN.path}/api/useraccess/registeruser`, userModel);
+        const response = await API.post(`${REACT_APP_API_URL}/api/useraccess/registeruser`, userModel);
         return response.data;
     }catch(error){
-        console.error('Błąd rejestracji użytkownika', error);
-        throw error;
+        console.error('Register user failed', error);
     }
 }
 
+//Sends request on user sign in
 export const loginUser = async (email: string, password: string) => {
     try{
-        const response = await API.post(`${DOMAIN.path}/api/useraccess/login`, {email, password});
+        const response = await API.post(`${REACT_APP_API_URL}/api/useraccess/login`, {email, password});
         return response.data;
     }catch(error){
-        console.error('Błąd logowania użytkownika', error);
-        throw error;
+        console.error('Sign in user failed', error);
+    }
+}
+
+//Downloads settings when user sings in
+export const getSettingsOnLogin = async () => {
+    try{
+        const response = await axios.get(`${REACT_APP_API_URL}/api/useraccess/userSettings`);
+        return response.data;
+    }
+    catch(error){
+        console.error('Get settings on login failed', error);
     }
 }
