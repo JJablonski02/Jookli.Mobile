73abe3392375b90b52cf7f715002d2178c8c8ec0
JJablonski02 <kuba2805@icloud.com>
2024-03-25 23:35:53 +0100

    settings preferences component

--- src/components/settings/SettingsPreferencesComponent.tsx
+++ src/components/settings/SettingsPreferencesComponent.tsx
@@ -5,7 +5,7 @@ import Spacing from "../../constants/Spacing";
 import Colors from "../../constants/Colors";
 import FontSize from "../../constants/FontSize";
 import SettingsDropdown from "./nestedComponents/SettingsDropdown";
-import SettingsDisplayProfitOne from "./nestedComponents/SettingsDisplayProfitOne";
+import SettingsDisplayProfitOne, { DisplayProfitFormatType } from "./nestedComponents/SettingsDisplayProfit";
 import SettingsDarkMode from "./nestedComponents/SettingsDarkMode";
 import SettingsAccountIdentifier from "./nestedComponents/SettingsAccountIdentifier";
 import SettingsTextInput from "./nestedComponents/SettingsTextInput";
@@ -15,13 +15,15 @@ import TextV from "../global/Text";
 import { SettingsSafeView } from "./nestedComponents/SettingsSafeView";
 import Languages from "../../../locales/options/languages.json";
 import TimeZones from "../../../locales/options/timezones.json";
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AccountStatus } from "../../core/enums/UserTypes";
 import { Loader } from "../global/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, setLoading } from "../../redux/store/store";
+import { getPreferences, postChangeEmail, postDisplayProfitAs, postLanguageAndTimeZone } from "../../api/endpoints/api-usersettings-service";
 import * as SecureStore from 'expo-secure-store';
+import { HttpStatusCode } from "axios";
+import { ValidateChangeEmailAddress } from "../../common/FieldValidatorProvider";
+import { NotifyError } from "../notifications/Notify";
 
 interface Props {
   label: string;
@@ -50,66 +52,127 @@ const fetchTimezoneData = () => {
   return data;
 };
 
-interface PreferencesDTO{
+interface PreferencesResponse{
   accountStatus: AccountStatus;
-  displayFormat: string;
+  displayFormat: number;
   language: string;
   timeZone: string;
+}
 
+interface DisplayFormatDTO{
+  displayFormat: DisplayProfitFormatType;
+}
+
+interface LanguageAndTimeZoneDTO{
+  language: string;
+  timeZone: string;
+}
+
+interface EmailDTO{
+  email: string;
 }
 
 
-
 const SettingsPreferencesComponent: React.FC = () => {
-  const [Email, setEmail] = React.useState<string>("");
-  const [preferences, setPreferences] = React.useState<PreferencesDTO>();
+  const [email, setEmail] = React.useState<EmailDTO>({email: ""});
+  const [preferences, setPreferences] = React.useState<PreferencesResponse>();
   const [showVerifyEmail, setShowVerifyEmail] = React.useState<boolean>(false);
-
+  const [displayFormat, setDisplayFormat] = React.useState<DisplayFormatDTO>();
+  const [languageAndTimeZone, setLanguageAndTimeZone] = React.useState<LanguageAndTimeZoneDTO>({
+    language: "",
+    timeZone: "",
+  })
+  const [accountStatus, setAccountStatus] = React.useState<AccountStatus>();
   const dispatch = useDispatch();
   const {isLoading} = useSelector((state : RootState) => state.appState);
-  const [refreshing, setRefreshing] = React.useState(false);
-const [userInfo, setUserInfo] = React.useState<string>();
+
   useEffect(() => {
-    const getData = async () => { 
-      const user = await AsyncStorage.getItem('useInfo');
-      setUserInfo(user || '');
+    const getAccountDetails = async () => {
+
+      const email = await SecureStore.getItemAsync("email");
+      const accountStatus = await SecureStore.getItemAsync('accountStatus');
+      if(email){
+        setEmail({email: email});
+      }
+      
+      if(accountStatus){
+        var accountStatusNumber = parseInt(accountStatus);
+        setAccountStatus(accountStatusNumber);
+        if(accountStatusNumber !== AccountStatus.Confirmed){
+          setShowVerifyEmail(true);
+        }else{
+          setShowVerifyEmail(false);
+        }
+      }
     };
 
     const fetchData = async () => {
-      try{
-      dispatch(setLoading(true));
-      const token = await SecureStore.getItemAsync('accessToken')
 
-       const response = await axios.get<PreferencesDTO>('/api/userSettings/preferences', {
-          headers:{
-            Authorization: `Bearer ${token}`,
-          }
-        });
-        setPreferences(response.data);
+        const response = await getPreferences();
+         setPreferences(response);
+    }
 
-        if(response.data.accountStatus !== AccountStatus.Confirmed){
-          setShowVerifyEmail(true);
-        }
-        dispatch(setLoading(false));
+    
 
-      }catch(ex){
-        console.log(ex);
-        dispatch(setLoading(false));
+    const fetchDataAndAccountDetails = async () => {
+      await dispatch(setLoading(true));
 
+      //Get account details from secure store
+      await getAccountDetails();
+
+      //Get account details from server
+      await fetchData();
+
+      await dispatch(setLoading(false));
+    }
+    
+    fetchDataAndAccountDetails();
+  }, []);
+
+  //If server response is not the same as the current account status, update the account status in the secure store.
+  useEffect(() => {
+    const updateAccountStatus = async () => {
+    if(preferences){
+      if(preferences.accountStatus !== accountStatus){
+        await SecureStore.setItemAsync('accountStatus', preferences.accountStatus.toString());
       }
     }
-    getData();
-    fetchData();
-  }, []);
+  }
+
+  updateAccountStatus();
+
+  }, [preferences])
+
+
+
+  const handleOnPressDisplayProfit = async () => {
+     await postDisplayProfitAs(displayFormat);
+  };
+
+  const handleOnPressLanguageAndTimeZone = async () => {
+    await postLanguageAndTimeZone(languageAndTimeZone);    
+  };
+
+  const handleOnPressChangeEmailAddress = async () => {
+    var message = ValidateChangeEmailAddress(email.email);
+    if(!message){
+      var response = await postChangeEmail(email);
+      if(response === HttpStatusCode.Ok){
+        SecureStore.setItemAsync("email", email.email);
+      }
+    }else{
+      NotifyError('Operation failed', message)
+    }
+  }
 
   return (
     <SettingsSafeView>
       <View style={styles.container}>
-        {!userInfo && <SettingsVerifyEmail />}
+        {showVerifyEmail && <SettingsVerifyEmail />}
         <View>
           <TextV style={styles.title}>Change email address</TextV>
-          <SettingsTextInput placeholder="Email" onChangeText={setEmail} />
-          <SettingsButtonSave onPress={() => "Email has been changed"} />
+          <SettingsTextInput placeholder="Email" onChangeText={(text: string) => setEmail({...email, email: text})} defaultText={email.email}/>
+          <SettingsButtonSave onPress={() => handleOnPressChangeEmailAddress()} />
         </View>
         <View>
           <TextV style={styles.title}>Language and time zone</TextV>
@@ -117,18 +180,22 @@ const [userInfo, setUserInfo] = React.useState<string>();
             label="Language"
             placeholder="Select language..."
             dataSource={fetchLanguageData()}
+            selectedValue={(text : string) => setLanguageAndTimeZone({...languageAndTimeZone, language: text})}
+            defaultValue={preferences?.language}
           />
           <SettingsDropdown
             label="Time zone"
             placeholder="Select time zone..."
             dataSource={fetchTimezoneData()}
+            selectedValue={(text : string) => setLanguageAndTimeZone({...languageAndTimeZone, timeZone: text})}
+            defaultValue={preferences?.timeZone}
           />
-          <SettingsButtonSave onPress={() => "Email has been changed"} />
+          <SettingsButtonSave onPress={() => handleOnPressLanguageAndTimeZone()} />
         </View>
         <View>
           <TextV style={styles.title}>Display profit as</TextV>
-          <SettingsDisplayProfitOne />
-          <SettingsButtonSave onPress={() => "Email has been changed"} />
+          <SettingsDisplayProfitOne value={(selectedType : any) => setDisplayFormat({...displayFormat, displayFormat: selectedType})} defaultValue={preferences?.displayFormat}/>
+          <SettingsButtonSave onPress={() => handleOnPressDisplayProfit()} />
         </View>
         <View>
           <TextV style={styles.title}>Dark mode</TextV>
@@ -136,10 +203,10 @@ const [userInfo, setUserInfo] = React.useState<string>();
         </View>
         <View>
           <TextV style={styles.title}>Account Identifier</TextV>
-          <SettingsAccountIdentifier />
+          <SettingsAccountIdentifier accountIdentifier={"1000-635-435"} />
         </View>
       </View>
-      {(userInfo) ?  
+      {isLoading ?  
             <Loader/>
              : null}
     </SettingsSafeView>
