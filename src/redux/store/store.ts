fca13ceae8252d5b5b11f02b0306937160feff4c
JJablonski02 <kuba2805@icloud.com>
2024-03-25 23:31:24 +0100

    redux adjustments

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,14 @@
 import { combineReducers, createStore } from "redux";
 import * as Device from 'expo-device';
 import NetInfo, { NetInfoState, NetInfoStateType } from "@react-native-community/netinfo";
+import { Appearance } from "react-native";
 
 const initialState = {
     isLoading: false,
     splashLoading: false,
     deviceInfo: {},
-    connectionInfo: {}
+    connectionInfo: {},
+    darkMode: false
 };
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -22,6 +24,8 @@ const rootReducer = combineReducers({
                         return {...state, splashLoading: action.payload};
                         case 'SET_LOADING':
                             return {...state, isLoading: action.payload};
+                            case 'SET_DARK_MODE':
+                                return {...state, darkMode: action.payload};
             default:
                 return state;
         }
@@ -38,19 +42,37 @@ export const setSplashLoading = (splashLoading: boolean) => ({
     payload: splashLoading
 });
 
+export const setDarkMode = (darkMode: boolean) => ({
+    type: 'SET_DARK_MODE',
+    payload: darkMode
+});
+
 export const store = createStore(rootReducer);
 
 export const saveCurrentDeviceInfo = async () => {
     try
     {
     const connectionInfo = await getConnectionInfo();
+    const darkMode = getApperanceInfo();
 
     store.dispatch({type: 'CURRENT_DEVICE_INFO', payload: deviceInfo})
     store.dispatch({type: 'CURRENT_CONNECTION_INFO', payload: connectionInfo})
+    store.dispatch({type: 'SET_DARKMODE', payload: darkMode})
+
     }
     catch(e){
         console.log(e);
     }
+}
+
+const getApperanceInfo = () =>{
+    const result = Appearance.getColorScheme();
+    if(result === 'dark'){
+        return true;
+    }else{
+        return false;
+    }
+
 }
  
 const getConnectionInfo = async (): Promise<NetInfoState> => {
