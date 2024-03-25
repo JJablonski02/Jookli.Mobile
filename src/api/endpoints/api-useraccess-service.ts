957aba5d924faba716d9c30da78a385422c21209
JJablonski02 <kuba2805@icloud.com>
2024-03-25 23:32:28 +0100

    api-useraccess-service

--- src/api/endpoints/api-useraccess-service.ts
+++ src/api/endpoints/api-useraccess-service.ts
@@ -2,7 +2,6 @@ import { API } from "../core/api-connection";
 import axios from "axios";
 import {RegisterUserDTO} from '@api';
 import { REACT_APP_API_URL } from "@env";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SecureStore from 'expo-secure-store';
 
 async function getAccessToken(){
@@ -54,6 +53,30 @@ export const getAnalyticsData: (dateFilter : string) => Promise<any> = async (da
         return response.data;
     }catch(error){
         console.error('Get analytics data failed', error);
+    }
+}
+
+export const getMainPageData: () => Promise<any> = async () => {
+    try{
+        const token = await getAccessToken();
+        const response = await axios.get(`api/details/mainPage`, {
+            headers:{
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        await SecureStore.setItem('accountStatus', response.data.accountStatus.toString());
+        return response.data;
+    }catch(error){
+        console.error('Get main page data failed', error);
+    }
+}
+
+export const postRecoverPassword: (email: string) => Promise<any> = async (email) => {
+    try{
+        const response = await axios.post(`/api/useraccess/recoverpassword`, {email});
+        return response.status;
+    }catch(error){
+        console.error('Recover password failed', error);
     }
 }
 
