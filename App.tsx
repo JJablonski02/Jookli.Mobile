1817b9ab319927113e64969daec6fa8e0c387221
JJablonski02 <kuba2805@icloud.com>
2024-03-25 23:32:05 +0100

    app tsx enter

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { AppRegistry, Appearance, StyleSheet, Text, View, useColorScheme } from "react-native";
 import { Navigation } from "./src/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import fonts from "./src/constants/Font";
@@ -15,18 +15,18 @@ import { SetDefaultAppLanguage } from "./services/i18nextDetector";
 import { Provider, useSelector } from "react-redux";
 import { store, saveCurrentDeviceInfo } from "./src/redux/store/store";
 import { useTranslation } from "react-i18next";
-import axios from "axios";
-import { REACT_APP_API_URL } from "@env";
+import { configureAxios } from "./src/api/core/api-connection";
 
 export default function App() {
 
   const {t} = useTranslation();
-
   const [loaded, error] = useFonts(fonts);
+  const theme = useColorScheme();
   useEffect(() => {
     saveCurrentDeviceInfo();
-    axios.defaults.baseURL = REACT_APP_API_URL;
-    axios.defaults.timeout = 5000;
+    configureAxios();
+    const isDarkTheme = theme === 'dark';
+    console.log(isDarkTheme);
 
     if (error) throw error;
   }, [error]);
